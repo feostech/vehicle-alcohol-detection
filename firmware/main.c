e980e8177b1705079cb3d329541673b520f21838
prasath <jothiprasathpa@gmail.com>
2024-02-14 15:20:26 +0530

    Adding main.c

--- firmware/main.c
+++ firmware/main.c
@@ -2,27 +2,28 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-#define ANALOG_PIN A0 // Define analog pin for Alcohol Sensor
-#define LED_PIN 16   //  Define GPIO pin for LED indication
+#define ANALOG_PIN A0
+#define LED_PIN 16
 
 const char* ssid = "SSID";
-const char* password = "SSID-Password";
-const char* mqtt_server = "IP-Address";  // MQTT Broker IP Address
-const uint16_t mqtt_server_port = 1884;   // MQTT Broker Port 
-const char* mqttTopic = "Topic-name";      // MQTT topic for Alcohol Sensor
+const char* password = "SSID-password";
+const char* mqtt_server = "192.168.1.14";
+const uint16_t mqtt_server_port = 1883;
+const char* mqttTopic = "Topic-name";
+const int detectionThreshold = 400;
+const char* vehicleNumber = "TN 36 D XXXX";
 
-WiFiClient wifiClient;  // Create an instance of WiFiClient for managing WiFi connection 
-PubSubClient mqttClient(wifiClient); // Create an instance of MQTTClient for communication using WiFiClient
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient); 
 
-// Function to Set up WiFi Connection
 void setup_wifi() {
-  delay(10);
+  delay(10); //Initial delay for stability
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(500); // Delay for 500 milliseconds before checking again
     Serial.print(".");
   }
   Serial.print("IP address: ");
@@ -30,7 +31,6 @@ void setup_wifi() {
   Serial.println("WiFi connected");
 }
 
-// Function to connect the MQTT broker
 void connect() {
   while (!mqttClient.connected()) {
     Serial.print("Attempting MQTT connection...");
@@ -46,6 +46,12 @@ void connect() {
   }
 }
 
+void printAlert(const char* vehicleNumber) {
+  Serial.print(" Vehicle ");
+  Serial.print(vehicleNumber);
+  Serial.println(" ALERT: Alcohol detected!");
+}
+
 void setup() {
   Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
@@ -57,16 +63,18 @@ void loop() {
   if (!mqttClient.connected()) {
     connect();
   }
-  int sensorValue = analogRead(ANALOG_PIN);  // Read analog value from alcohol sensor
-  if (sensorValue > 400) {  // Check if alcohol level is above a threshold or not 
+  int sensorValue = analogRead(ANALOG_PIN);
+  if (sensorValue > detectionThreshold) {
     digitalWrite(LED_PIN, LOW); // Turn ON the LED 
-    Serial.print("Value = ");
+    Serial.print(" Value = ");
     Serial.print(sensorValue);
-    Serial.println(" ALERT: Alcohol detected!");
-    mqttClient.publish(mqttTopic, ("ALERT: Alcohol detected!")); // Publish alert message to MQTT 
+    printAlert(vehicleNumber);
+    String alertMessage = "ALERT: Alcohol detected in Vehicle ";
+    alertMessage += vehicleNumber;
+    mqttClient.publish(mqttTopic, alertMessage.c_str());
   } else {
     digitalWrite(LED_PIN, HIGH); // Turn OFF the LED
   }
   mqttClient.loop();
-  delay(1000);
+  delay(1000); // Delay for 1 second before next iteration
 }
