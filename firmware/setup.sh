91da9f87582d289109f608ef475c412de7d62b40
Thowfeek <mdthowf19@yahoo.com>
2024-02-17 19:45:14 +0530

    changes in setup.sh

--- firmware/setup.sh
+++ firmware/setup.sh
@@ -2,9 +2,15 @@
 
 set -e 
 
+ESP32_URL="https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json"
+ESP8266_URL="https://arduino.esp8266.com/stable/package_esp8266com_index.json"
+
 #Install Arduino CLI
-sudo snap install arduino-cli 
-sudo snap connect arduino-cli:raw-usb 
+cd /usr/bin/
+sudo apt-get install curl
+
+#Add arduino-cli to the system path
+cd /home/usr/arduino-cli/
 
 #Verify the installation by running
 arduino-cli version
@@ -12,17 +18,13 @@ arduino-cli version
 #Create a configuration file
 arduino-cli config init
 
-# Set up additional board URLs (esp32)
-arduino-cli config add board_manager.additional_urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
-
-# Set up additional board URLs (esp8266)
-arduino-cli core update-index --additional-urls https://arduino.esp8266.com/stable/package_esp8266com_index.json
-
 # update the index
-arduino-cli core update-index
+arduino-cli core update-index --additional-urls=$ESP32_URL
+arduino-cli core update-index --additional-urls=$ESP8266_URL
 
 #install the core for your board
-arduino-cli core install arduino:avr
+arduino-cli core install esp32:esp32
+arduino-cli core install esp8266:esp8266
 
 #verify we have installed the core properly by running
 arduino-cli core list
