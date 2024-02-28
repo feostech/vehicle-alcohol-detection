ccdbb3e6bf09925cd6584d0048777e22f376a1f4
Thowfeek <mdthowf19@yahoo.com>
2024-02-28 19:16:58 +0530

    Fixing esp8266 in flash.sh file

--- firmware/flash.sh
+++ firmware/flash.sh
@@ -3,16 +3,20 @@
 
 ARDUINO_CLI=$HOME/bin/arduino-cli
 BOARD_REGEX='[[:alnum:]]\+:[[:alnum:]]\+:[-_[:alnum:]]\+'
-BOARD=$($ARDUINO_CLI board list | grep -o  $BOARD_REGEX)
-PORT="/dev/ttyACM0"
+BOARD=$($ARDUINO_CLI board list | grep -o  $BOARD_REGEX) 
+#If esp8266 board not detected automatically ,we give manual board name for esp8266
+if [ "$BOARD" = "" ]; then
+	BOARD=esp8266:esp8266:nodemcuv2
+fi
+PORT="/dev/ttyUSB0"
 
-# Install Dependent libraries
+#Install Dependent libraries
 $ARDUINO_CLI lib install pubsubclient &&
-
+    
 # Compile the sketch
 $ARDUINO_CLI compile  --export-binaries -b $BOARD . &&
 
 # Upload the compiled sketch to the board
-$ARDUINO_CLI upload -p $PORT --fqbn $BOARD .
+$ARDUINO_CLI upload -p $PORT --fqbn $BOARD . &&
  
-echo "Arduino flashed successfully!"
+echo "Arduino flashed successfully!" 
